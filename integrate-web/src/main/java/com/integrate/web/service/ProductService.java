4b2d7b40838742e4c2c541e765b6abd481e5af3d
lam.yu <lam.yu@onebank.com.cn>
2018-09-17 23:48:52 +0800

    init

---
+++ integrate-web/src/main/java/com/integrate/web/service/ProductService.java
@@ -0,0 +1,35 @@
+package com.integrate.web.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.integrate.model.Image;
+import com.integrate.model.Product;
+import com.integrate.web.dao.ImageDao;
+import com.integrate.web.dao.ProductDao;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ImageDao imageDao;
+	
+	@Autowired
+	private ProductDao productDao;
+
+	public List<Map<String,Object>> getProducts() {
+		return productDao.getProducts();
+	}
+	
+	public Product getProduct(Long id) {
+		Product product =  productDao.getProduct(id);
+		if(product != null){
+			List<Image> images = imageDao.getImages(product.getId());
+			product.setImages(images);
+		}
+		return product;
+	}
+}
